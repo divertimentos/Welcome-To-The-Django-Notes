c69fd0d2709c8464c889cdf1290d17ea22dd3caa
guiemi <guiemi.teixeira@gmail.com>
2019-01-21 21:31:55 -0200

    Finaliza a aula Mais do que strings

---
+++ M1A11_mais_do_que_strings/divertimento.py
@@ -0,0 +1,19 @@
+nome = 'Henrique'
+print(type(nome))
+
+print(nome.encode())
+print('henriquê'.encode())
+
+print(nome.upper())
+print(nome.capitalize())
+
+print("Henrique Bastos".title())
+
+nome = "henrique bastos"
+print(nome.replace("s", "th"))
+
+print(nome.split())
+print(nome.split("e"))
+print(" ".join(["Henrique", "Bastos"]))
+
+print(len(nome))
