2c19d35d8a23736f58df7015699cdb681123c02b
guiemi <guiemi.teixeira@gmail.com>
2019-03-29 17:33:03 -0300

    Adiciona o arquivo de anotações-base da aula sobre os básicos do Django

---
+++ M1A23_conheca_o_django/divertimentos.py
@@ -0,0 +1,15 @@
+"""
+O Django é um framework web de alto nível escrito em Python que possibilita rápido desenvolvimento e design pragmático.
+
+Permite que o programador não se preocupe com muitos detalhes de baixo nível.
+
+É escrito em Python: ele continua a cultura do Python. Ele é uma extensão natural, com o propósito web, do Python.
+
+Rápido desenvolvimento != Desenvolvimento rápido. Significa que o desenvolvimento possui ciclos curtos de feedback.
+
+Design pragmático: ele se preocupa em resolver boa parte dos problemas inerentes a frameworks web, deixando que o programador resolva o que é necessário. Possui várias camadas.
+
+ 
+
+ 
+"""
