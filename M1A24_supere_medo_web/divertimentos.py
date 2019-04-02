f1128e19bd42ea2600f143781a61a9133e2b2cd9
guiemi <guiemi.teixeira@gmail.com>
2019-04-02 10:56:31 -0300

    Finaliza a aula sobre superar o medo da web

---
+++ M1A24_supere_medo_web/divertimentos.py
@@ -0,0 +1,20 @@
+"""
+Domain Name Serice (DNS)
+
+Internet Protocol (IP)
+
+Transmission Control Protocol (TCP)
+
+***
+
+Stateless x Stateful
+
+- O HTTP é um protocolo stateless
+ - Significa que toda a informação sobre o cliente está presente na mensagem enviada na requisição ao servidor
+
+- O FTP, por exemplo, é um protocolo stateful
+ - Se a conexão cai, você refaz todo o processo
+
+O stateless é mais vantajoso porque é mais rápido e dinâmico.
+
+"""
