0d2d4f551053daf930d976f9b2394fdd037980d6
guiemi <guiemi.teixeira@gmail.com>
2019-04-24 12:29:24 -0300

    Apply black formatting

--- Divertimentos/basic/list1.py
+++ Divertimentos/basic/list1.py
@@ -28,6 +28,7 @@ def match_ends(words):
             counter += 1
     return counter
 
+
 # B. front_x
 # Given a list of strings, return a list with the strings
 # in sorted order, except group all the strings that begin with 'x' first.
@@ -35,6 +36,8 @@ def match_ends(words):
 # ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
+
+
 def front_x(words):
     # +++your code here+++
     return
@@ -55,37 +58,43 @@ def sort_last(tuples):
 # what each function returns vs. what it's supposed to return.
 def test(got, expected):
     if got == expected:
-        prefix = ' OK '
+        prefix = " OK "
     else:
-        prefix = '  X '
-    print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+        prefix = "  X "
+    print("%s got: %s expected: %s" % (prefix, repr(got), repr(expected)))
 
 
 # Calls the above functions with interesting inputs.
 def main():
-    print('match_ends')
-    test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
-    test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
-    test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+    print("match_ends")
+    test(match_ends(["aba", "xyz", "aa", "x", "bbb"]), 3)
+    test(match_ends(["", "x", "xy", "xyx", "xx"]), 2)
+    test(match_ends(["aaa", "be", "abc", "hello"]), 1)
 
     print()
-    print('front_x')
-    test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
-         ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
-    test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
-         ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
-    test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
-         ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+    print("front_x")
+    test(
+        front_x(["bbb", "ccc", "axx", "xzz", "xaa"]),
+        ["xaa", "xzz", "axx", "bbb", "ccc"],
+    )
+    test(
+        front_x(["ccc", "bbb", "aaa", "xcc", "xaa"]),
+        ["xaa", "xcc", "aaa", "bbb", "ccc"],
+    )
+    test(
+        front_x(["mix", "xyz", "apple", "xanadu", "aardvark"]),
+        ["xanadu", "xyz", "aardvark", "apple", "mix"],
+    )
 
     print()
-    print('sort_last')
-    test(sort_last([(1, 3), (3, 2), (2, 1)]),
-         [(2, 1), (3, 2), (1, 3)])
-    test(sort_last([(2, 3), (1, 2), (3, 1)]),
-         [(3, 1), (1, 2), (2, 3)])
-    test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
-         [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+    print("sort_last")
+    test(sort_last([(1, 3), (3, 2), (2, 1)]), [(2, 1), (3, 2), (1, 3)])
+    test(sort_last([(2, 3), (1, 2), (3, 1)]), [(3, 1), (1, 2), (2, 3)])
+    test(
+        sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+        [(2, 2), (1, 3), (3, 4, 5), (1, 7)],
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
